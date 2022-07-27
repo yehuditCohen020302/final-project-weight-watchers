93c3dd87708a5d47f533e0462ce780b2e48da17b
yehuditCohen020302 <yehudit8824@gmail.com>
2022-07-27 16:08:58 +0300

    success to getAllUsers from the mongo

--- client-side-react/react-project-side/src/api/manager.js
+++ client-side-react/react-project-side/src/api/manager.js
@@ -1,43 +1,14 @@
-const baseUrl = "https://pacific-headland-08901.herokuapp.com/";
+// const baseUrl = `http://localhost:3000/`;
 
  export function getUsers() {
-    debugger
-    fetch(baseUrl+"users")
+    
+    fetch(`http://localhost:3000/users`)
       .then((response) => response.json())
       .then((response) => console.log(response))
     //    .then((response) => { (usersTo = response)})
-      .then((response) => drawTable(response))
-      .catch((err) => {
+
+    .catch((err) => {
         console.log(err);
       });
 
-}
-
- function drawTable(users) {
-    debugger
-    console.log(users);
-    const container = document.querySelector(".usersTable");
-    container.innerHTML = "";
-    let table = "";
-
-    users.forEach((user) => {
-      let bmi =
-        user.weightsHistory[user.weightsHistory.length - 1].weight /
-        Math.pow(user.height, 2);
-      let c = "green";
-      if (
-        user.weightsHistory[user.weightsHistory.length - 1].weight >
-        user.weightsHistory[user.weightsHistory.length - 2].weight
-      ) {
-        c = "red";
-      }
-      table += `
-                <tr>
-                    <th>${user.firstName + " " + user.lastName}</th>
-                    <th style="color:${c}">${Math.floor(bmi * 100) / 100}</th>
-                    <th><a href="../html/userPage.html?userId=${user.id}">details user</a></th>
-                </tr>`;
-    });
-    
-    container.innerHTML += table;
 }
