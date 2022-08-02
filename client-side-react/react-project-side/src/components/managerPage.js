c75e056e693b8c0696bbfd1a7b4cf198510004b1
chaniYehudit <p0548453160@gmail.com>
2022-08-02 23:48:08 +0300

    the manager table is working!!

--- client-side-react/react-project-side/src/components/managerPage.js
+++ client-side-react/react-project-side/src/components/managerPage.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+// import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,65 +6,74 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import  { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import getUsers from "../api/manager"
 import { ClickAwayListener } from '@mui/material';
-debugger
 
-function createData(firstName, lastName, address, email, phone, hieght) {
-    return { firstName, lastName, address, email, phone, hieght };
+function createData(firstName, lastName, city, street, number, email, phone, hieght) {
+    return { firstName, lastName, city, street, number, email, phone, hieght };
 }
 
-export default async function  BasicTable() {
+export default function BasicTable() {
 
-     const [allUsers, setAllUsers] = useState([]);
+    const [allUsers, setAllUsers] = useState([]);
     const [rows, setRows] = useState([]);
-    const [rows11, setRows11] = useState("");
-    // try { setAllUsers(getUsers); }
-    // catch (e) { console.error(e); }
-    var users =await getUsers();
-console.log(rows11+" rows")
-    console.log(users);
+
+    useEffect(() => {
+
+        const getUsers = async () => {
+            var response = await fetch(`http://localhost:3000/users`)
+            if (response.status === 200 && response.ok) {
+                var data = await response.json()
+            }
+            setAllUsers(data);
+        }
+        getUsers()
+    }, []);
+    console.log(rows + " rows")
+    console.log(allUsers);
+
+    const newRow = []
     debugger
-    users.length!==0?setRows11(
-        // ()=>{
-        //     users.map((row) => (
-        //     createData(row.firstName, row.lastName, row.address, row.email, row.phone, row.hieght)
-        // ))}
-        "jjj"
-    ):<></>
+    allUsers.map((row) => (
+        newRow.push(createData(row.firstName, row.lastName, row.city, row.street, row.houseNumber, row.emailAddress, row.phoneNumber, row.height))
+    ))
+    console.log(newRow)
 
     return (
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell>Dessert (100g serving)</TableCell>
-                        <TableCell align="right">firstName</TableCell>
-                        <TableCell align="right">lastName</TableCell>
-                        <TableCell align="right">address</TableCell>
-                        <TableCell align="right">phone</TableCell>
-                        <TableCell align="right">email</TableCell>
-                        <TableCell align="right">hieght</TableCell>
+                        <TableCell align="center">firstName</TableCell>
+                        <TableCell align="center">lastName</TableCell>
+                        <TableCell align="center">city</TableCell>
+                        <TableCell align="center">street</TableCell>
+                        <TableCell align="center">houseNumber</TableCell>
+                        <TableCell align="center">email</TableCell>
+                        <TableCell align="center">phone</TableCell>
+                        <TableCell align="center">hieght</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {rows.length!==0 ? <div>
-                        {rows.map((row) => (
-                            <TableRow
-                                key={row.name}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {row.firstName}
-                                </TableCell>
-                                <TableCell align="right">{row.lastName}</TableCell>
-                                <TableCell align="right">{row.address}</TableCell>
-                                <TableCell align="right">{row.phone}</TableCell>
-                                <TableCell align="right">{row.email}</TableCell>
-                                <TableCell align="right">{row.hieght}</TableCell>
-                            </TableRow>
-                        ))}</div> : <div>null</div>}
+                    {newRow.map((row) => (
+                        <TableRow
+                            key={row.name}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }} >
+                            {/* <TableCell component="th" scope="row">
+                                {row.firstName}
+                            </TableCell> */}
+                            <TableCell align="center">{row.firstName}</TableCell>
+                            <TableCell align="center">{row.lastName}</TableCell>
+                            <TableCell align="center">{row.city}</TableCell>
+                            <TableCell align="center">{row.street}</TableCell>
+                            <TableCell align="center">{row.number}</TableCell>
+                            <TableCell align="center">{row.email}</TableCell>
+                            <TableCell align="center">{row.phone}</TableCell>
+                            <TableCell align="center">{row.hieght}</TableCell>
+                        </TableRow>
+                    ))}
+
                 </TableBody>
             </Table>
         </TableContainer>
